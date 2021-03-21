ca1c936e457f3e5c211a9fada7a8e2368de63004
radkmb <rad.kmb@gmail.com>
2021-03-21 19:28:56 +0900

    Allowed logout

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -3,19 +3,26 @@
     <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
   
     <ul class="navbar-nav ml-auto">
-  
+
+      @guest
       <li class="nav-item">
-        <a class="nav-link" href="">ユーザー登録</a>
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
       </li>
-  
+      @endguest
+
+      @guest
       <li class="nav-item">
         <a class="nav-link" href="">ログイン</a>
       </li>
-  
+      @endguest
+
+      @auth
       <li class="nav-item">
         <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
       </li>
-  
+      @endauth
+
+      @auth
       <!-- Dropdown -->
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
@@ -33,9 +40,11 @@
           </button>
         </div>
       </li>
-      <form id="logout-button" method="POST" action="">
-      </form>
-      <!-- Dropdown -->
+    <form id="logout-button" method="POST" action="{{ route('logout') }}">
+      @csrf
+    </form>
+    <!-- Dropdown -->
+    @endauth
   
     </ul>
   
